5fa84d0cbc89ccf9e0825940232405123746b888
MicrosoftLearner <sidd.tale@live.in>
2019-09-20 19:56:45 +0530

    Default OnlineTshirtDesigningWebAPI

---
+++ OnlineTshirtDesigningWebAPI/OnlineTshirtDesigningWebAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace OnlineTshirtDesigningWebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ OnlineTshirtDesigningWebAPI/OnlineTshirtDesigningWebAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace OnlineTshirtDesigningWebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
